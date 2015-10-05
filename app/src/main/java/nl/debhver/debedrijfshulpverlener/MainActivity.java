155aac9a31a6b733f0774bbc256e6a345a67b62a
Snitch <13031740@student.hhs.nl>
2015-10-05 13:26:02 +0200

    second commit
    second commit

--- app/src/main/java/nl/debhver/debedrijfshulpverlener/MainActivity.java
+++ app/src/main/java/nl/debhver/debedrijfshulpverlener/MainActivity.java
@@ -14,10 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        ParseObject testObject = new ParseObject("TestObject");
-        testObject.put("foo", "bar");
-        testObject.saveInBackground();
+        
 
     }
 
