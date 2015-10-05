80766397160bf6aead41034b97cbad58da76abdd
TvEgmond <timvngmnd@gmail.com>
2015-10-05 13:29:24 +0200

    test2

--- app/src/main/java/nl/debhver/debedrijfshulpverlener/parse/ParseApplication.java
+++ app/src/main/java/nl/debhver/debedrijfshulpverlener/parse/ParseApplication.java
@@ -3,15 +3,6 @@ package nl.debhver.debedrijfshulpverlener.parse;
 import android.app.Application;
 
 import com.parse.Parse;
-import com.parse.ParseObject;
-
-import nl.debhver.debedrijfshulpverlener.model.Branch;
-import nl.debhver.debedrijfshulpverlener.model.Equipment;
-import nl.debhver.debedrijfshulpverlener.model.EvacuationPlan;
-import nl.debhver.debedrijfshulpverlener.model.ImageModel;
-import nl.debhver.debedrijfshulpverlener.model.Incident;
-import nl.debhver.debedrijfshulpverlener.model.Training;
-import nl.debhver.debedrijfshulpverlener.model.User;
 
 public class ParseApplication extends Application {
 
@@ -20,14 +11,6 @@ public class ParseApplication extends Application {
         super.onCreate();
         // Enable Local Datastore.
         Parse.enableLocalDatastore(this);
-
-        ParseObject.registerSubclass(Branch.class);
-        ParseObject.registerSubclass(ImageModel.class);
-        ParseObject.registerSubclass(Equipment.class);
-        ParseObject.registerSubclass(User.class);
-        ParseObject.registerSubclass(EvacuationPlan.class);
-        ParseObject.registerSubclass(Incident.class);
-        ParseObject.registerSubclass(Training.class);
 
         Parse.initialize(this, "f7TM8VHqrH17pUmlzPSgq567RryuYOjvnB96FC0P", "vdB7HpL58bnYc6TOij3NolTyq72PDh32fXiwYjiL");
     }
