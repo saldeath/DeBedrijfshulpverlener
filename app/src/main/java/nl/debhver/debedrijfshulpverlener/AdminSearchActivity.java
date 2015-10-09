ad4f4df79e1f81e7cf7d72453bf53961c66e8972
knmr <koennm@gmail.com>
2015-10-09 14:24:15 +0200

    Searching users works

--- app/src/main/java/nl/debhver/debedrijfshulpverlener/AdminSearchActivity.java
+++ app/src/main/java/nl/debhver/debedrijfshulpverlener/AdminSearchActivity.java
@@ -2,6 +2,12 @@ package nl.debhver.debedrijfshulpverlener;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
+
+import nl.debhver.debedrijfshulpverlener.models.User;
 
 /**
  * Created by Koen on 6-10-2015.
@@ -11,21 +17,29 @@ public class AdminSearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_search_users);
+        retrieveUsers();
+    }
 
+    public void retrieveUsers(){
+        DBManager.getInstance().getUsers(this);
     }
 
 
+    public void populateListView(List<User> userList){
+        ListView userListView = (ListView)findViewById(R.id.userListView);
 
-    public void searchUsers(){
-        /*
-        userToCreate.setName();
-        userToCreate.setBranch();
-        userToCreate.setEmail("");
-        userToCreate.setEROFunction();
-        userToCreate.setRight();
-        userToCreate.setTelephoneNumber();
-        */
+        ArrayAdapter<User> adapter = new ArrayAdapter<User>(this, android.R.layout.simple_spinner_dropdown_item, userList);
+
+        userListView.setAdapter(adapter);
+
+
+
+
+
+  //      List<User> items = (ArrayList)userList;
+
+  //      ArrayAdapter<User> adapter = new ArrayAdapter<User>(this, android.R.layout.simple_list_item_1, items);
+
     }
-
 
 }
