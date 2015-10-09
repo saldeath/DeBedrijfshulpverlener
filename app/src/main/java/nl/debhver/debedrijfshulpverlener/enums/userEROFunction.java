8b6dd54e6b9267ec6b5edc851cbe210ace7fcdfb
knmr <koennm@gmail.com>
2015-10-09 14:55:06 +0200

    - None toegevoegd

--- app/src/main/java/nl/debhver/debedrijfshulpverlener/enums/userEROFunction.java
+++ app/src/main/java/nl/debhver/debedrijfshulpverlener/enums/userEROFunction.java
@@ -8,7 +8,7 @@ import nl.debhver.debedrijfshulpverlener.parse.ParseApplication;
  */
 public enum UserEROFunction {
     FIRE(R.string.user_ERO_function_fire), FIRST_AID(R.string.user_ERO_function_first_aid),
-    AED(R.string.user_ERO_function_AED);
+    AED(R.string.user_ERO_function_AED), NONE(R.string.user_ERO_function_none);
 
     private final int resourceId;
 
