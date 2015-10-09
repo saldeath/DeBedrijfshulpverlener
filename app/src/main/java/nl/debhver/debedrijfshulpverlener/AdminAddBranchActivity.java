f860422941d137c4f8379a537d2fb161c0675c74
timmeijers <tim-meijers@hotmail.com>
2015-10-09 14:17:29 +0200

    Tim M Update: Add branch

---
+++ app/src/main/java/nl/debhver/debedrijfshulpverlener/AdminAddBranchActivity.java
@@ -0,0 +1,85 @@
+package nl.debhver.debedrijfshulpverlener;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
+import nl.debhver.debedrijfshulpverlener.models.Branch;
+
+public class AdminAddBranchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_add_branch);
+    }
+
+    public boolean checkFields(){
+        EditText tempField;
+        tempField = (EditText)findViewById(R.id.inputName);
+
+        if(tempField.getText().toString().isEmpty()){
+            this.popupShortToastMessage(getString(R.string.error_empty_name));
+            return false;
+        }
+
+        tempField = (EditText)findViewById(R.id.inputCity);
+        if(tempField.getText().toString().isEmpty()){
+            this.popupShortToastMessage(getString(R.string.error_empty_telephone));
+            return false;
+        }
+
+        tempField = (EditText)findViewById(R.id.inputPostalCode);
+        if(tempField.getText().toString().isEmpty()){
+            this.popupShortToastMessage(getString(R.string.error_empty_email));
+            return false;
+        }
+
+        tempField = (EditText)findViewById(R.id.inputAddress);
+        if(tempField.getText().toString().isEmpty()){
+            this.popupShortToastMessage(getString(R.string.error_empty_password));
+            return false;
+        }
+
+        else {
+            return true;
+        }
+    }
+
+    void popupShortToastMessage(String msg){
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+    }
+
+    public void addBranch(){
+        if(checkFields()){
+            Branch branch = new Branch();
+            EditText tempField;
+
+            tempField = (EditText)findViewById(R.id.inputName);
+            branch.setName(tempField.getText().toString());
+
+            tempField = (EditText)findViewById(R.id.inputCity);
+            branch.setCity(tempField.getText().toString());
+
+            tempField = (EditText)findViewById(R.id.inputPostalCode);
+            branch.setZipCode(tempField.getText().toString());
+
+            tempField = (EditText)findViewById(R.id.inputAddress);
+            branch.setAddress(tempField.getText().toString());
+
+            DBManager.getInstance().createBranch(branch, this);
+        }
+    }
+
+    public void clearFieldsAfterAddingBranch() {
+        EditText tempField;
+        tempField = (EditText) findViewById(R.id.inputName);
+        tempField.setText("");
+        tempField = (EditText) findViewById(R.id.inputCity);
+        tempField.setText("");
+        tempField = (EditText) findViewById(R.id.inputPostalCode);
+        tempField.setText("");
+        tempField = (EditText) findViewById(R.id.inputAddress);
+        tempField.setText("");
+    }
+}
