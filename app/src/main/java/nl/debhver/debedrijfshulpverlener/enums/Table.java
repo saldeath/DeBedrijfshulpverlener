60a7ff50dc2e2f1921f2f367ad9b9db3df31dc4c
TvEgmond <timvngmnd@gmail.com>
2015-10-16 12:56:15 +0200

    equipment update

---
+++ app/src/main/java/nl/debhver/debedrijfshulpverlener/enums/Table.java
@@ -0,0 +1,20 @@
+package nl.debhver.debedrijfshulpverlener.enums;
+
+/**
+ * Created by timvn on 10/16/15.
+ */
+public enum Table {
+    USER("user"), BRANCH("branch"), EQUIPMENT("equipment"), INCIDENT("incident"), TRAINING("training"),
+    EVACUATIONPLAN("evacuation_plan"), IMAGE("image"), USERTRAINING("user_training");
+
+    private final String tableName;
+
+    private Table(String tableName) {
+        this.tableName = tableName;
+    }
+
+    @Override
+    public String toString() {
+        return tableName;
+    }
+}
